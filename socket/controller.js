ea76c1488c58e37a77511586fc97f03ae7e6c63c
RoxanaCamara <roxana.camara07@gmail.com>
2022-11-01 18:01:17 -0300

    feat: comentario

--- socket/controller.js
+++ socket/controller.js
@@ -3,6 +3,7 @@ const socketController = (socket) => {
     
     socket.on('enviar-mensaje', (payload, callback) => {
         callback(socket.id)
+        /*Envia lo que contenga ese obj payload a todos los clientes conectados, excepto a quien lo envio */
         socket.broadcast.emit('enviar-mensaje', payload)
     })
 
