2b253837015c294080ff37456f106f70274747d7
RoxanaCamara <roxana.camara07@gmail.com>
2022-11-05 14:43:14 -0300

    feat: socket ticket

--- models/ticketControl.js
+++ models/ticketControl.js
@@ -1,3 +1,14 @@
+const path = require('path')
+const fs = require('fs')
+
+class Ticket {
+    constructor(numero, escritorio){
+        this.numero=numero
+        this.escritorio=escritorio
+    }
+}
+
+
 class TicketControl {
     
     constructor(){
@@ -15,6 +26,33 @@ class TicketControl {
             this.tickets= tickets;
             this.ultimos4= ultimos4;
         }
+    }
+
+    guardarDB(){
+        const dbpath = path.join(__dirname, '../data.json')
+        fs.writeFileSync(dbpath, JSON.stringify(this.toJson()))
+    }
+
+    siguiente(){
+        this.ultimo+=1
+        const ticket =  new Ticket(this.ultimo, null)
+        this.tickets.push(ticket)
+        this.guardarDB()
+        return ticket.numero
+    }
+
+    atenderTicket( escritorio ){
+        if(this.tickets.length == 0){
+            return null
+        }
+        const ticket = this.tickets.unshift()
+        ticket.escritorio= escritorio
+        this.ultimos4.unshift(ticket)
+        if( this.ultimos4.length > 4){
+            this.ultimos4.splice(-1,1)
+        }
+        this.guardarDB()
+        return ticket
     }
 
     get toJson() {
